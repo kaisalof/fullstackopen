128747386b61f3f9b587521c3970d1583119ef23
kaisalof <kaisalofman@live.com>
2021-07-14 09:44:34 +0300

    step3 1.8

--- src/App.js
+++ src/App.js
@@ -6,11 +6,25 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.all}</p>
+      <p>average {((props.good * 1) + (props.neutral * 0) + (props.bad * (-1))) / props.all}</p>
+      <p>positive {props.good / props.all} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] =useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
@@ -31,13 +45,13 @@ const App = () => {
       <Button handleClick={handleGoodClick} text='good' />
       <Button handleClick={handleNeutralClick} text='neutral' />
       <Button handleClick={handleBadClick} text='bad' />
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {((good * 1) + (neutral * 0) + (bad * (-1))) / all}</p>
-      <p>positive {good / all} %</p>
+
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+      />
     </div>
   )
 }
