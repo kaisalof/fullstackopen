0f177bc720626e57cb6d038894770c8cdfdad7cd
kaisalof <kaisalofman@live.com>
2021-07-21 19:01:50 +0300

    2.8 puhelinluettelo step3

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231244' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     if (persons.some(person => person.name === newName)) {
@@ -17,12 +19,15 @@ const App = () => {
     } else {
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
-
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,6 +41,12 @@ const App = () => {
           />
         </div>
         <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
@@ -45,6 +56,7 @@ const App = () => {
           {persons.map(person =>
             <tr key={person.name}>
               <td>{person.name}</td>
+              <td>{person.number}</td>
             </tr>
           )}
         </tbody>
